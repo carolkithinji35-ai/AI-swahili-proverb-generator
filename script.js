40d00b18a573329cbbd3e05515da2cc2b047d3e2
carolkithinji35-ai <carolkithinji35@gmail.com>
2025-12-29 23:24:04 +0300

    Import Axios and Typewriter plugin

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-function getProverb(response) {}
+function getProverb(response) {
+  let proverbs = document.querySelector(".response");
+  proverbs.classList.add("show");
+
+ new Typewriter(".response", {
+   strings: response.data.answer,
+   autoStart: true,
+   loop: false,
+   delay: 40,
+   cursor: null,
+   
+ });
+}
 
 function generateProverb(event) {
   event.preventDefault();
   let proverbs = document.querySelector(".response");
-  proverbs.innerHTML =
-    "Haba na haba hujaza kibaba. (Little by little fills the pot.)";
+  proverbs.innerHTML = "Generating a Swahili proverb... Please wait.";
+  proverbs.classList.add("show");
 
-  let apiKey = "";
-  let apiUrl = "";
+  const apiKey = "568925f93ofcc31043b0b8154ebt149a";
   let context = "";
   let prompt =
-    "Generate a Swahili proverb along with its English translation. Provide only the proverb and its translation without any additional text.";
-  
-  axios.get(apiUrl). then(getProverb);
+    "Generate a Swahili proverb along with its English translation. Provide only the proverb and its translation without any additional text. Display the text in the following html format: 'Swahili Proverb' - 'English Translation'.Make sure to indicate the translation clearly.";
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(getProverb);
 }
 let btn = document.querySelector("button");
 btn.addEventListener("click", generateProverb);
